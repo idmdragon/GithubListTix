6cdeb566ee38d63b293668c80d3731780188664a
Ilham Dwi <ilham.uklison@gmail.com>
2022-03-10 19:52:58 +0700

    feat(api_key): Remove Api Key

--- app/src/main/java/com/idmdragon/githublist/utils/Constant.kt
+++ app/src/main/java/com/idmdragon/githublist/utils/Constant.kt
@@ -1,6 +1,6 @@
 package com.idmdragon.githublist.utils
 
 object Constant {
-    const val API_KEY = "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"
+    const val API_KEY = "API_KEY"
     const val BASE_URL = "https://api.github.com/"
 }
